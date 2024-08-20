c6c239ab0dded60b299d26131d63c1aeb005f3a8
shidiqi-coding <shidiqiihsan@gmail.com>
2024-08-20 12:05:09 +0700

    Add files via upload

---
+++ src/loader.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    
+
+
+    loader.classList.add("loader-hidden");
+
+    loader.addEventListener("transitioned", () => {
+        document.body.removeChild("loader");
+    })
+})
